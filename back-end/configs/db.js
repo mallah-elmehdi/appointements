99282ef0e9068c861bed45a13c3824d1e3ad7c06
mallah-elmehdi <elmehdimallah@gmail.com>
2025-12-26 22:50:30 +0800

    DB connection .env bypass

--- back-end/configs/db.js
+++ back-end/configs/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 // MONGODB CONNECTION
 mongoose
-    .connect(process.env.DB_CONNECTION)
+    .connect('mongodb://127.0.0.1:27017/cleanAndSealDentalProgram')
     .then(() => {
         console.log('MongoDB connected');
     })
